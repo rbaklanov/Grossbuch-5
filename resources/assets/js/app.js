99cde89e632f31f2499b773ce5ce9c17e8c1278d
rbaklanov <rbaklanov@gmail.com>
2017-08-09 16:53:08 +0300

    Configured JS and CSS and setup database

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,6 +8,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Buefy from 'buefy';
+vue.use(Buefy);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,7 +17,7 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+// Vue.component('example', require('./components/Example.vue'));
 
 const app = new Vue({
     el: '#app'
